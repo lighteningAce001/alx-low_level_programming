3f6b0e986b8780085ed9ab85cb3e90751f85ad8f
Timothy <timothychinweike@gmail.com>
2022-06-06 08:57:49 -0700

    printf is here

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints tht content of the function.
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+retun (0);
+}
