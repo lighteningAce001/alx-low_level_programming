61d329d829ececec88730f3d7856e2e923aa03fd
Timothy <timothychinweike@gmail.com>
2022-06-08 00:43:47 -0700

    Hotfix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,12 @@
-(11 sloc)  189 Bytes
-
-   
-
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
-*
 * main - Entry point
-*
 * Return:1
-*
 */
-
 int main(void)
-
 {
-
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-
 return (1);
-
 }
