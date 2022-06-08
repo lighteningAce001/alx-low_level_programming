edf3d3271633b646c7d1ed785d4c09e9e95807df
Timothy <timothychinweike@gmail.com>
2022-06-08 00:38:58 -0700

    additional tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+(11 sloc)  189 Bytes
+
+   
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+*
+* main - Entry point
+*
+* Return:1
+*
+*/
+
+int main(void)
+
+{
+
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+return (1);
+
+}
