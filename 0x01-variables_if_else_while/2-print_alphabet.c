0595279943a6dd6c30e52ca1425e0cc53b71f0ed
Timothy <timothychinweike@gmail.com>
2022-06-08 18:20:28 -0700

    alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints the alphabets in lower case
+* Return: 0
+*/
+int main(void)
+{
+char utc;
+for (utc = 'a'; utc <= 'z'; utc++)
+{
+putchar(utc);
+}
+putchar('/n');
+return (0);
+}
